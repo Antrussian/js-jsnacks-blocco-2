98dbf83d680892e0f62b75c877930aebde52e51b
Antrussian <antonino.russo.agent@gmail.com>
2023-11-17 11:34:07 +0100

    Fine Esercizio

--- snacks-1/js/script.js
+++ snacks-1/js/script.js
@@ -8,19 +8,23 @@ Continua a chiedere i numeri all'utente
 
 
  let numeri = [];
- let somma = 0;
+let somma = 0;
+
+while (somma < 50) {
+    let numeroInserito = parseFloat(prompt("Inserisci un numero:"));
+
+    
+    numeri.push(numeroInserito);
+
+    somma += numeroInserito;
+
  
- while (somma < 50) {
-     let numeroInserito = parseFloat(prompt("Inserisci un numero:"));
- 
- 
-     if (somma + numeroInserito <= 50) {
-         numeri.push(numeroInserito);
-         somma += numeroInserito;
-     } else {
-         console.log("Il numero inserito supera 50.");
-     }
- }
- 
- console.log("La somma degli elementi ha superato 50.:", numeri);
+    if (somma >= 50) {
+        console.log("La somma degli elementi ha superato 50.");
+       
+    }
+}
+
+console.log("Array finale:", numeri);
+
  
