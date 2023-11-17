e3594b8c658227d268a33c67b0d8a74822e0d9b2
Antrussian <antonino.russo.agent@gmail.com>
2023-11-17 11:58:19 +0100

    Upload file

---
+++ snacks-2/js/script.js
@@ -0,0 +1,6 @@
+/*
+Generatore di "nomi cognomi" casuali: il Grande Gatsby ha una lista di nomi e una lista di cognomi, 
+e da questi due array distinti vuole generare una falsa lista di invitati con nome e cognome.
+*/
+
+
