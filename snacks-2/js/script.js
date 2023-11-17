436308d3c3e5156e71c94d6b3477654c8f69c528
Antrussian <antonino.russo.agent@gmail.com>
2023-11-17 12:13:23 +0100

    Fine Esercizio

--- snacks-2/js/script.js
+++ snacks-2/js/script.js
@@ -4,3 +4,22 @@ e da questi due array distinti vuole generare una falsa lista di invitati con no
 */
 
 
+
+let nomi = ['Mario', 'Luigi', 'Giovanna', 'Sofia', 'Alessio', 'Francesca'];
+let cognomi = ['Rossi', 'Bianchi', 'Ferrari', 'Esposito', 'Ricci', 'Gallo'];
+
+
+let listaInvitati = [];
+
+for (let i = 0; i < 6; i++) {
+    let indiceNome = Math.floor(Math.random() * nomi.length);
+    let indiceCognome = Math.floor(Math.random() * cognomi.length);
+
+    let nomeCasuale = nomi[indiceNome];
+    let cognomeCasuale = cognomi[indiceCognome];
+
+    let invitatoCasuale = nomeCasuale + ' ' + cognomeCasuale;
+    listaInvitati.push(invitatoCasuale);
+}
+
+console.log("Lista Falsa di Invitati:", listaInvitati);
