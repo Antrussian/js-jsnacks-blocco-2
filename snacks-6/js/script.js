0f32a2c9b9e4e48a4bc777405a59e0b3f693b835
Antrussian <antonino.russo.agent@gmail.com>
2023-11-21 12:14:01 +0100

    Upload file

---
+++ snacks-6/js/script.js
@@ -0,0 +1,23 @@
+/*Chiedo all'utente un numero
+e genero attraverso una funzione un numero di quadrati equivalente al numero inserito dall'utente nel DOM,
+ in qualsiasi forma. */
+
+
+ /*Chiedo all'utente un numero */
+
+ let numberChoice = parseInt (prompt ('Inserisci un numero'));
+
+
+
+
+ function generaSquare (numeroIndice) {
+
+    for (let i = 0 ; i <= numeroIndice ; i++ )
+
+    document.createElement('div');
+
+
+ }
+
+
+ /*funzione che genera quadrati modifica indice for */
