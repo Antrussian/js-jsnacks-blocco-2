d4af7b058c0a51a513c91e5ea5bdfc4f6e848ee4
Antrussian <antonino.russo.agent@gmail.com>
2023-11-24 12:32:21 +0100

    Creati oggetti

---
+++ snacks-8/js/script.js
@@ -0,0 +1,51 @@
+/* Crea 10 oggetti che rappresentano una zucchina, 
+indicandone per ognuno varietà, peso e lunghezza.
+ Calcola quanto pesano tutte le zucchine */
+
+
+
+let zucchine = [
+
+  { varietà: 'Zucchina1', 
+  peso: 150, 
+  lunghezza: 20 },
+
+  { varietà: 'Zucchina2', 
+  peso: 120,    
+  lunghezza: 18 },
+
+  { varietà: 'Zucchina3',
+   peso: 130,
+   lunghezza: 22 },
+
+  { varietà: 'Zucchina4', 
+  peso: 110, 
+  lunghezza: 16 },
+
+  { varietà: 'Zucchina5', 
+  peso: 140, 
+  lunghezza: 24 },
+
+  { varietà: 'Zucchina6', 
+  peso: 160, 
+  lunghezza: 26 },
+
+  { varietà: 'Zucchina7', 
+  peso: 100, 
+  lunghezza: 14 },
+
+  { varietà: 'Zucchina8', 
+  peso: 180, 
+  lunghezza: 28 },
+
+  { varietà: 'Zucchina9', 
+  peso: 170, 
+  lunghezza: 25 },
+
+  { varietà: 'Zucchina10',
+   peso: 200, 
+   lunghezza: 30 }
+
+];
+
+
